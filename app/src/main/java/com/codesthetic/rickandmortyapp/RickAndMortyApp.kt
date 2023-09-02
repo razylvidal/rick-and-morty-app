a65f58c847d06437f139fa97cff977020a856e42
Razyl Vidal <razylabbygail.vidal@unc.edu.ph>
2023-09-02 21:36:54 +0800

    Initial commit

---
+++ app/src/main/java/com/codesthetic/rickandmortyapp/RickAndMortyApp.kt
@@ -0,0 +1,8 @@
+package com.codesthetic.rickandmortyapp
+
+/**
+ * Created by razylvidal on September 02, 2023
+ */
+class RickAndMortyApp {
+
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,21 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Rick And Morty App"
+include(":app")
+include(":engine-core")
+include(":feature-characters")
+include(":feature-locations")
+include(":feature-episodes")
