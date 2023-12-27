fc24637b1c27af30acb775e2884547aedcc7cfd3
Razyl Abbygail Vidal <razyl.vidal@nueca.com.ph>
2023-12-27 15:55:22 +0800

    Refactor GetChractersUseCase

--- engine-core/src/main/java/com/codesthetic/engine/core/characters/domain/usecases/GetCharactersUseCase.kt
+++ engine-core/src/main/java/com/codesthetic/engine/core/characters/domain/usecases/GetCharactersUseCase.kt
@@ -6,11 +6,11 @@ import com.codesthetic.engine.core.characters.domain.CharacterGateway
 /**
  * Created by razylvidal on September 09, 2023
  */
-class GetCharacterUseCase(
+class GetCharactersUseCase(
     private val gateway: CharacterGateway
 ) {
 
-    suspend fun get(id: Int): Character {
-        return gateway.get(id)
+    suspend fun get(): List<Character> {
+        return gateway.get()
     }
 }
